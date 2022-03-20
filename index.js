6f450475501f852cb31d50861c7fadaed6559cf7
GOGU EDUKONDAL <97788201+GoguEdukondal2468@users.noreply.github.com>
2022-03-20 18:24:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+function sum(){
+    let sumOfResult = 5 + 5
+    console.log(sumOfResult)
+}
+
+sum()
+
+function subtract(num1,num2){
+    let subtractionOfResult = num1  -  num2
+    console.log(subtractionOfResult)
+
+}
+subtract(2,1)
+
+function multiply(num1,num2){
+    let multiplicationOfResult = num1 * num2
+    console.log(multiplicationOfResult)
+}
+multiply(3,4)
+
+
+function division(num1,num2){
+    let divisionOfResult = num1/num2
+    console.log(divisionOfResult)
+
+}
+division(10,2)
+
+function square(num1,num2){
+    let squaringOfResult = num1*num1+2*num1*num2+num2*num2
+    console.log(squaringOfResult)
+
+}
+square(2,3)
